1cf7529bf82b88159cf460c03a60dcc300d0ac5e
mtighorbani <mahdi1381.ghorbani@gmail.com>
2024-10-20 14:26:40 +0330

    feat: make main page and chat form

--- app/components/chatForm/ChatForm.tsx
+++ app/components/chatForm/ChatForm.tsx
@@ -3,27 +3,46 @@ import { generateCompletion } from "@/app/api/open-ai";
 import answerResponseSelector from "@/store/ApiResponse/apiResponseSelector";
 import useAppDispatch from "@/store/storeUtils/useAppDipatch";
 import useAppSelector from "@/store/storeUtils/useAppSelector";
-import React, { useState } from "react";
+import React from "react";
+import Input from "../base/Input";
+import chatInputSelector from "@/store/inputText/chatInputSelector";
 
 const ChatForm = () => {
   const dispatch = useAppDispatch();
   const answers = useAppSelector(answerResponseSelector().answerContent);
+  const input = useAppSelector(chatInputSelector().userInput);
 
   return (
-    <>
-      <div className="flex justify-center items-center  h-56 bg-gray-100">
+    <div className="  h-screen mx-10">
+      <div className="flex justify-center items-center  h-28 bg-gray-100">
         <h1 className="text-3xl font-bold text-blue-600">Hello, Next!</h1>
         <button
           className=" w-48 rounded-xl h-10 bg-blue-800 text-white mx-5"
-          onClick={() => generateCompletion(dispatch)}
+          onClick={() =>
+            generateCompletion({ dispatch: dispatch, userPrompt: input })
+          }
         >
           Make Req
         </button>
       </div>
-      <h1 className="w-[50%] flex mx-auto text-blue-700 text-xs font-extrabold">
-        {answers.map((x) => x.message.content)}
-      </h1>
-    </>
+
+      <div className="chat chat-start">
+        <div className="chat-bubble max-w-[70%]">{input}</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble">
+          {answers.map((x) => x.message.content)}
+        </div>
+      </div>
+      <Input
+        type="text"
+        placeholder="اینجا بنویس"
+        className="input w-full  my-10"
+        action={() =>
+          generateCompletion({ dispatch: dispatch, userPrompt: input })
+        }
+      />
+    </div>
   );
 };
 
